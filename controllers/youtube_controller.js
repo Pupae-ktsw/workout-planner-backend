8451e6c6314e2bcd9a924ec0499634bb6a326acc
Pupae-Punthtia <punthita.kw@gmail.com>
2023-03-30 19:18:06 +0700

    edit response from youtube API

--- controllers/youtube_controller.js
+++ controllers/youtube_controller.js
@@ -1,8 +1,10 @@
 const asyncHandler = require('express-async-handler');
 const axios = require('axios');
 const validator = require('validator');
+const YoutubeVideo = require('../models/youtubeVideo_model');
 const youtubeApiKey = process.env.YOUTUBE_API_KEY;
 const youtubeApiURL = 'https://www.googleapis.com/youtube/v3';
+const ytDuration = require('youtube-duration');
 
 // @desc    search video on YouTube
 // @route   GET /search
@@ -12,8 +14,32 @@ const searchVid = asyncHandler(async (req, res) => {
     const url = `${youtubeApiURL}/search?key=${youtubeApiKey}&type=video&part=snippet&q=${searchQuery}`;
     const response = await axios.get(url);
     if(response.status == 200){
-        res.status(200).json(response.data.items);
+        
+        const items = response.data.items;
+        console.log(`body: ${items}`);
+        const searchResults = [];
+        for(var item of items){
+            console.log(`item: ${item}`);
+            let clipUrl = `https://youtu.be/${item.id.videoId}`;
+            console.log(`clipURL: ${clipUrl}`);
+            let clipImg = `https://i3.ytimg.com/vi/${item.id.videoId}/maxresdefault.jpg`
+            console.log(`clipImg: ${clipImg}`);
+            let clip = YoutubeVideo({
+                url: clipUrl,
+                thumbnail: clipImg,
+                title: item.snippet.title,
+                channel: item.snippet.channelTitle,
+                duration: 1800
+            });
+            searchResults.push(clip);
+
+        }
+
+        console.log(`search: ${searchResults}`);
+
+        res.status(200).json(searchResults);
     }
+    
 });
 
 module.exports = { searchVid }
