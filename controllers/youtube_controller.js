72b7185bae07dc9fa99d9b917cc70ea282ab7824
Pupae-Punthtia <punthita.kw@gmail.com>
2023-03-31 14:06:35 +0700

    add playlist search

--- controllers/youtube_controller.js
+++ controllers/youtube_controller.js
@@ -11,19 +11,16 @@ const ytDuration = require('youtube-duration');
 // @access  Private
 const searchVid = asyncHandler(async (req, res) => {
     const searchQuery = req.query.search_query;
-    const url = `${youtubeApiURL}/search?key=${youtubeApiKey}&type=video&part=snippet&q=${searchQuery}`;
-    const response = await axios.get(url);
+    const searchUrl = `${youtubeApiURL}/search?key=${youtubeApiKey}&type=video,playlist&part=snippet&q=${searchQuery}`;
+    const response = await axios.get(searchUrl);
     if(response.status == 200){
-        
         const items = response.data.items;
-        console.log(`body: ${items}`);
         const searchResults = [];
+        var videoIds = '';
         for(var item of items){
-            console.log(`item: ${item}`);
+            videoIds += item.id.videoId + ',';
             let clipUrl = `https://youtu.be/${item.id.videoId}`;
-            console.log(`clipURL: ${clipUrl}`);
             let clipImg = `https://i3.ytimg.com/vi/${item.id.videoId}/maxresdefault.jpg`
-            console.log(`clipImg: ${clipImg}`);
             let clip = YoutubeVideo({
                 url: clipUrl,
                 thumbnail: clipImg,
@@ -32,11 +29,16 @@ const searchVid = asyncHandler(async (req, res) => {
                 duration: 1800
             });
             searchResults.push(clip);
-
         }
+        // videoIds = videoIds.slice(0,-1);
+        // console.log(`videoIds: ${videoIds}`);
+        // // https://www.googleapis.com/youtube/v3/videos?id=<videoId1,videoId2>&part=contentDetails&key={YOUR_API_KEY}
+        // const durationUrl = `${youtubeApiURL}/videos?id=${videoIds}&part=contentDetails&key=${youtubeApiKey}`;
+        // console.log(`durationUrl: ${durationUrl}`);
+        // const responseDuration = await axios.get(durationUrl);
+        // if (responseDuration.status == 200){
 
-        console.log(`search: ${searchResults}`);
-
+        // }
         res.status(200).json(searchResults);
     }
     
